bf445046cc9befe01785016837363fe6be319119
koushikabalusu <koushika1204@sasi.ac.in>
2022-07-19 15:59:51 +0530

    changes

---
+++ Assistedpracticeproject/src/tyepcasting.java
@@ -0,0 +1,16 @@
+public class tyepcasting {
+
+	public static void main(String[] args) {
+		
+		String input = "12";
+		
+		//convert the string input above into appropriate primitive type
+		
+		byte mybyte = Byte.parseByte(input);
+		System.out.println("Byte val = " + mybyte);
+		
+		int myint = Integer.parseInt(input);
+		System.out.println("Integer val = " + myint);
+		
+	}
+}
